44d754e98aaabafbad5a0ab0d4893e5698dd3874
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-31 14:11:29 -0500

    still working

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,11 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts 
+    validates :name, uniqueness: true
+    validates :age, numericality: {greater_than_or_equal_to: 1}  
+    validates :bio, length: {minimum: 31}
 
+    def most_popular_post
+        self.posts.max_by {|post| post.likes}
+    end
 end
